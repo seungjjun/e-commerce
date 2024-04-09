d75d3e6f025167260bbe7b754a9d019c9eb428f3
seungjjun <il1uso@naver.com>
2024-04-09 14:08:44 +0900

    #11 test PaymentController 유닛 테스트 추가
    - 결제 요청 금액 네이밍 변경 (amount -> payAmount)
    - RequestBody 요청 필드값 검증을 위한 유닛 테스트 코드 추가

--- src/main/java/com/hanghae/ecommerce/api/dto/request/PaymentRequest.java
+++ src/main/java/com/hanghae/ecommerce/api/dto/request/PaymentRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.NotNull;
 
 public record PaymentRequest(
         @NotNull Long orderId,
-        @NotNull Long amount,
+        @NotNull Long payAmount,
         @NotBlank String paymentMethod) {
 }
