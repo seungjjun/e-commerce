a3787375e099149473dbcb8ae16de6c3ef7ff3ae
seungjjun <il1uso@naver.com>
2024-04-11 23:56:19 +0900

    #noissue test 주문 상태 변경 유닛 테스트 작성

--- src/test/java/com/hanghae/ecommerce/domain/order/OrderTest.java
+++ src/test/java/com/hanghae/ecommerce/domain/order/OrderTest.java
@@ -1,7 +1,24 @@
 package com.hanghae.ecommerce.domain.order;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.hanghae.ecommerce.Fixtures;
+import com.hanghae.ecommerce.storage.order.OrderStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class OrderTest {
 
+    @Test
+    @DisplayName("주문 상태 변경 테스트")
+    void changeOrderStatus() {
+        // Given
+        Order order = Fixtures.order(OrderStatus.READY);
+
+        // When
+        Order changedOrderStatus = order.changeStatus(OrderStatus.PAID);
+
+        // Then
+        assertThat(changedOrderStatus.orderStatus()).isEqualTo(OrderStatus.PAID.toString());
+    }
 }
