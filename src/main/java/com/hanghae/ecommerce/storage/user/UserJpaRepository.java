172d6f733ef3c6a4b0a2edf3e98e96fb6bbcab86
seungjjun <il1uso@naver.com>
2024-04-11 20:18:07 +0900

    #19 feat 주문 및 결제 동시성 처리
    - 비관적 락을 통해 주문 및 결제 시 동시성 처리
    - 동시성 테스트를 위한 OrderUseCase 통합 테스트

--- src/main/java/com/hanghae/ecommerce/storage/user/UserJpaRepository.java
+++ src/main/java/com/hanghae/ecommerce/storage/user/UserJpaRepository.java
@@ -1,6 +1,12 @@
 package com.hanghae.ecommerce.storage.user;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<UserEntity> findById(Long userId);
 }
