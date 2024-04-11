2dae1571d36ab62160b3698bc49a1ccd28219a49
seungjjun <il1uso@naver.com>
2024-04-11 22:47:46 +0900

    #noissue chore swagger 설정

--- src/main/java/com/hanghae/ecommerce/api/controller/ProductController.java
+++ src/main/java/com/hanghae/ecommerce/api/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.hanghae.ecommerce.api.dto.response.ProductListResponse;
 import com.hanghae.ecommerce.api.dto.response.ProductSummaryResponse;
 import com.hanghae.ecommerce.domain.product.Product;
 import com.hanghae.ecommerce.domain.product.ProductCoreService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Tag(name = "상품 목록 조회 API", description = "등록된 상품 목록을 조회하는 API")
     @GetMapping
     public ProductListResponse getProducts() {
         List<Product> products = productService.getProducts();
@@ -31,12 +33,14 @@ public class ProductController {
         return new ProductListResponse(productSummaryResponseList);
     }
 
+    @Tag(name = "상품 세부 정보 조회 API", description = "상품 세부 정보를 조회하는 API")
     @GetMapping("/{id}")
     public ProductDetailResponse getProduct(@PathVariable Long id) {
         Product product = productService.getProductDetail(id);
         return ProductDetailResponse.from(product);
     }
 
+    @Tag(name = "인기 상품 조회 API", description = "최근 3일간 가장 많이 팔린 상품 목록을 조회하는 API")
     @GetMapping("/popular")
     public ProductListResponse popularProducts() {
         List<Product> products = productService.getPopularProducts();
