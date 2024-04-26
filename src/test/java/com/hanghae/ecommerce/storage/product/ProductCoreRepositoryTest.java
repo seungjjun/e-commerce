e5df3af73ce92e6a6abdf7e2310fe2611c443d3d
seungjjun <il1uso@naver.com>
2024-04-26 14:16:40 +0900

    #noissue feat jacoco 의존성 추가 및 테스트 환경 분리
    #36 refactor CI 실행 시 테스트 커버러지 조건 추가
    
    - BRANCH 커버리지 100%
    - LINE 커버리지 70%
    
    #36 refactor business layer 중점으로 테스트 커버리지 검사
    
    - business layer인 domain 패키지의 라인 커버리지 100% 으로 설정
    - ecs task definition의 image는 ecr에서 가져온 이미지값 사용

--- src/test/java/com/hanghae/ecommerce/storage/product/ProductCoreRepositoryTest.java
+++ src/test/java/com/hanghae/ecommerce/storage/product/ProductCoreRepositoryTest.java
@@ -7,29 +7,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ProductCoreRepositoryTest {
-    @Autowired
-    private ProductCoreRepository productCoreRepository;
+	@Autowired
+	private ProductCoreRepository productCoreRepository;
 
-    @Test
-    @Transactional
-    @DisplayName("상품 조회, 업데이트, 재고 확인을 통합 테스트")
-    void testProductLifecycle() {
-        // 상품 조회
-        Long productId = 1L;
-        Product product = productCoreRepository.findById(productId).get().toProduct();
-        assertThat(product.name()).isEqualTo("후드티");
-        assertThat(product.stockQuantity()).isEqualTo(5L);
+	@Test
+	@Transactional
+	@DisplayName("상품 조회, 업데이트, 재고 확인을 통합 테스트")
+	void testProductLifecycle() {
+		// 상품 조회
+		Long productId = 1L;
+		Product product = productCoreRepository.findById(productId).get().toProduct();
+		assertThat(product.name()).isEqualTo("후드티");
+		assertThat(product.stockQuantity()).isEqualTo(5L);
 
-        // 재고 업데이트
-        Product updatedStock = product.updateStock(3L);
-        productCoreRepository.updateStock(updatedStock);
+		List<Product> products = productCoreRepository.findByIdIn(List.of(productId));
+		assertThat(products.size()).isEqualTo(1);
+		assertThat(products.get(0).name()).isEqualTo("후드티");
 
-        // 업데이트된 재고 확인
-        product = productCoreRepository.findById(productId).get().toProduct();
-        assertThat(product.stockQuantity()).isEqualTo(3L);
-    }
+		List<Product> productAll = productCoreRepository.findAll();
+		assertThat(productAll.size()).isEqualTo(6);
+
+
+		// 재고 업데이트
+		Product updatedStock = product.updateStock(3L);
+		productCoreRepository.updateStock(updatedStock);
+
+		// 업데이트된 재고 확인
+		product = productCoreRepository.findById(productId).get().toProduct();
+		assertThat(product.stockQuantity()).isEqualTo(3L);
+	}
 }
